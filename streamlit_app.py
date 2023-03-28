8f184429ac3b0e91ea7e8728454ba42820acee02
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-03-28 20:18:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,39 +24,19 @@ st.image("image.jpg")
 
 st.sidebar.header('Input Features of The Transaction')
 
-sender_name = st.sidebar.text_input("""Input Sender ID""")
-receiver_name = st.sidebar.text_input("""Input Receiver ID""")
-step = st.sidebar.slider("""Number of Hours it took the Transaction to complete: """)
-types = st.sidebar.subheader(f"""
-                 Enter Type of Transfer Made:\n\n\n\n
-                 0 for 'Cash In' Transaction\n 
-                 1 for 'Cash Out' Transaction\n 
-                 2 for 'Debit' Transaction\n
-                 3 for 'Payment' Transaction\n  
-                 4 for 'Transfer' Transaction\n""")
-types = st.sidebar.selectbox("",(0,1,2,3,4))
-x = ''
-if types == 0:
-    x = 'Cash in'
-if types == 1:
-    x = 'Cash Out'
-if types == 2:
-    x = 'Debit'
-if types == 3:
-    x = 'Payment'
-if types == 4:
-    x =  'Transfer'
+sender_name = 1
+receiver_name = 2
+
+types = 4
+
     
-amount = st.sidebar.number_input("Amount in $",min_value=0, max_value=110000)
-oldbalanceorg = st.sidebar.number_input("""Sender Balance Before Transaction was made""",min_value=0, max_value=110000)
-newbalanceorg= st.sidebar.number_input("""Sender Balance After Transaction was made""",min_value=0, max_value=110000)
-oldbalancedest= st.sidebar.number_input("""Recipient Balance Before Transaction was made""",min_value=0, max_value=110000)
-newbalancedest= st.sidebar.number_input("""Recipient Balance After Transaction was made""",min_value=0, max_value=110000)
+amount = 4
+oldbalanceorg = 5
+newbalanceorg= 6
+oldbalancedest=7
+newbalancedest= 8
 isflaggedfraud = 0
-if amount >= 200000:
-  isflaggedfraud = 1
-else:
-  isflaggedfraud = 0
+
 
 
 
@@ -94,7 +74,7 @@ st.header(f'Credit request result for client {client_id}')
 # if pred == 1:
 #   st.error('Crédit Refusé')
     
-    
+step = client_id  
     
     
     
@@ -109,7 +89,7 @@ st.header(f'Credit request result for client {client_id}')
   
 if st.button("Detection Result"):
     values = {
-    "step": client_id,
+    "step": step,
     "types": types,
     "amount": amount,
     "oldbalanceorig": oldbalanceorg,
@@ -123,7 +103,7 @@ if st.button("Detection Result"):
     st.write(f"""### These are the transaction details:\n
     Sender ID: {sender_name}
     Receiver ID: {receiver_name}
-    1. Number of Hours it took to complete: {step}\n
+    1. Client Id is: {step}\n
     2. Type of Transaction: {x}\n
     3. Amount Sent: {amount}$\n
     4. Sender Balance Before Transaction: {oldbalanceorg}$\n
