51374de9244427958b61d7b4c4be5dcf23bc3086
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-03-28 22:29:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,12 @@ if st.button("Detection Result"):
                 """)
 
     res = re.post(f"https://creditcard2-production.up.railway.app/predict",json=values)
+
     json_str = json.dumps(res.json())
+    
+        
+    st.write(json_str)
+    st.write(type(json_str))
     resp = json.loads(json_str)
     
     prediction = resp
