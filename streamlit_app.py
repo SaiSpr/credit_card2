856cf9158d3bf9a77ac454d60d0f46aae4edba0e
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-03-28 21:03:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,7 +113,7 @@ if st.button("Detection Result"):
     st.write(prediction)
     st.write(type(prediction))
     
-    pred = prediction["prediction"]
+#     pred = prediction["prediction"]
 
     probability_value_0 = round(prediction["probability_0"] * 100,2)
     probability_value_1 = round(prediction["probability_1"] * 100,2)
